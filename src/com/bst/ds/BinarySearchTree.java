973ad465efea81c4534b8754a56e45f0ea57a9b2
me.nadeem55@gmail.com <me.nadeem55@gmail.com>
2022-10-18 11:32:07 +0530

    [Added: Searching Element in Binary Search Tree]

--- src/com/bst/ds/BinarySearchTree.java
+++ src/com/bst/ds/BinarySearchTree.java
@@ -51,16 +51,60 @@ public class BinarySearchTree {
 		inOrderRec(root.right);
 	}
 
+	public int size(Node root) {
+		if (root == null)
+			return 0;
+		else
+			return (size(root.left) + 1 + size(root.right));
+	}
+
+	public boolean search(Node root, int input) {
+		if (root == null) {
+			return false;
+		}
+
+		if (root.data == input) {
+			return true;
+		}
+
+		boolean result1 = search(root.left, input);
+		if (result1 == true)
+			return true;
+
+		boolean result2 = search(root.right, input);
+		return result2;
+
+	}
+
 	public static void main(String[] args) {
 		BinarySearchTree tree = new BinarySearchTree();
 		tree.insert(56);
-		tree.inOrder();
-		System.out.println();
 		tree.insert(30);
+		tree.insert(70);
+		tree.insert(22);
+		tree.insert(40);
+		tree.insert(11);
+		tree.insert(3);
+		tree.insert(16);
+		tree.insert(60);
+		tree.insert(95);
+		tree.insert(65);
+		tree.insert(63);
+		tree.insert(52);
 		tree.inOrder();
 		System.out.println();
-		tree.insert(70);
-		tree.inOrder();
+		int size = tree.size(tree.root);
+		if (size == 13)
+			System.out.println("All elements are added");
+		else
+			System.out.println("Missing some elements");
+
+		// checking data present or not
+		if (tree.search(tree.root, 52) != false) {
+			System.out.println("52 is present");
+		} else
+			System.out.println("52 is not  present");
+
 	}
 
 }
